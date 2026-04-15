9bd3f8e59f2cdd6e7c6303d3a48a34bc9f56c265
Phạm Tiến Thành <phamtienthanh1907.dc@gmail.com>
2026-04-15 17:19:04 +0700

    data_understanding.sql

--- sql/data_understanding.sql
+++ sql/data_understanding.sql
@@ -1,1 +1,15 @@
+-- =====================================
+-- SECTION 2: DATA UNDERSTANDING
+-- =====================================
 
+SELECT COUNT(*) AS total_rows FROM events;
+
+SELECT DISTINCT event_type FROM events;
+
+SELECT COUNT(DISTINCT visitor_id) AS total_users FROM events;
+
+SELECT COUNT(DISTINCT item_id) AS total_products FROM events;
+
+SELECT event_type, COUNT(*) AS total
+FROM events
+GROUP BY event_type;
