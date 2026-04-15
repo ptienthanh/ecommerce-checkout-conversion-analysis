bc8e260d4db7184fd522ae58961bba1e13e28a77
Phạm Tiến Thành <phamtienthanh1907.dc@gmail.com>
2026-04-15 18:29:15 +0700

    category_analysis.sql

--- sql/category_analysis.sql
+++ sql/category_analysis.sql
@@ -21,3 +21,9 @@ SELECT
 FROM product_funnel
 WHERE view_users > 50   -- filter noise (quan trọng)
 ORDER BY conversion_rate DESC;
+
+ORDER BY conversion_rate DESC
+LIMIT 5;
+
+ORDER BY conversion_rate ASC
+LIMIT 5; 
